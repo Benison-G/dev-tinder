13617d89ae169887a049c566e546a919772e0717
Benison-G <sachinlg60@gmail.com>
2026-06-24 20:25:08 +0530

    Multiple route handlers

--- src/app.js
+++ src/app.js
@@ -3,9 +3,25 @@ const express = require("express");
 
 const app = express();
 
+app.use("/multiple-route", (req, res, next) => {
+    console.log("handling first route")
+    next();
+}, (req, res) => {
+    console.log("handling second route")
+    res.send("Second response");
+})
+
 app.get("/hello", (req, res) => {
     res.send("Hello from the server from hello route.....")
 })
+
+app.get("/user-with-query", (req, res) => {
+    res.json({ message: "Query param is", query: req.query });
+});
+
+app.get("/user-with-param/:userId", (req, res) => {
+    res.json({ message: "Route param is", param: req.params.userId });
+});
 
 app.get("/user", (req, res) => {
     res.send({ firstName: "Nuthan", lastName: "Mithra" })
