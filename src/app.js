a155c24b6e62e20772cec112795cebc1de332c7b
Benison-G <sachinlg60@gmail.com>
2026-06-24 19:25:41 +0530

    Exploring multiple http methods

--- src/app.js
+++ src/app.js
@@ -7,6 +7,14 @@ app.get("/hello", (req, res) => {
     res.send("Hello from the server from hello route.....")
 })
 
+app.get("/user", (req, res) => {
+    res.send({ firstName: "Nuthan", lastName: "Mithra" })
+})
+
+app.post("/user", (req, res) => {
+    res.send("Saved successfully");
+})
+
 app.listen(5000, () => {
     console.log("Server is listening on port 5000...");
 })
