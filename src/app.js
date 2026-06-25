3733e78ccf05b187b68585a495e65a92434cd6e6
Benison-G <sachinlg60@gmail.com>
2026-06-25 18:39:44 +0530

    Schema validation or sanitation

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.post("/signup", async (req, res) => {
         await user.save();
         res.send("User created successfully");
     } catch (err) {
-        res.send("Could not added the user!!!")
+        res.send(err.message)
     }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,46 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 4
     },
     lastName: {
         type: String
     },
     email: {
-        type: String
+        type: String,
+        required: true,
+        lowercase: true,
+        trim: true
     },
     password: {
-        type: String
+        type: String,
+        required: true
     },
     age: {
-        type: String
+        type: Number,
+        min: 18
     },
     gender: {
-        type: String
+        type: String,
+        validate (value) {
+            if (!["male", "female", "others"].includes(value)) {
+                throw new Error("Specify correct gender!!")
+            }
+        }
     },
-});
+    photoURL: {
+        type: String,
+        default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOtWierCXpDqUvrbuoP4drf8g5rMQUqoGD3igeWN81-g&s=10"
+    },
+    about: {
+        type: String,
+        default: "This is my default about data!"
+    },
+    skills: {
+        type: [String]
+    }
+}, { timestamps: true });
 
 module.exports = mongoose.model("User", userSchema);
