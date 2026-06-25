ced9c5170508b3037d24b163f8f1182faa873177
Benison-G <sachinlg60@gmail.com>
2026-06-25 22:31:43 +0530

    Validator addition

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = mongoose.Schema({
     firstName: {
@@ -13,7 +14,12 @@ const userSchema = mongoose.Schema({
         type: String,
         required: true,
         lowercase: true,
-        trim: true
+        trim: true,
+        validate (value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Invalid email address ", value)
+            }
+        }
     },
     password: {
         type: String,
