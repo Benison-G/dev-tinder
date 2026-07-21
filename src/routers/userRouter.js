41d6c218dba460257fa40ff4eba753421e950e43
Benison-G <sachinlg60@gmail.com>
2026-07-21 14:55:29 +0530

    feed api

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,20 +1,85 @@
 const express = require("express");
 const { authMiddleWare } = require("../middlewares/auth");
-const ConnectionRequest  = require("../models/connectionRequest");
+const ConnectionRequest = require("../models/connectionRequest");
+const user = require("../models/user");
 
 const userRouter = express.Router();
 
 userRouter.get("/user/connections/recieved", authMiddleWare, async (req, res) => {
-    const logggedInUser = req.user;
+    const loggedInUser = req.user;
     const connections = await ConnectionRequest.find({
-        toUserId: logggedInUser._id,
+        toUserId: loggedInUser._id,
         status: "interested"
     }).populate("fromUserId", "firstName lastName");
 
-    res.send({ 
-        message: "Fetched connections successfully",
+    res.send({
+        message: "Fetched connections requests successfully",
         data: connections
     })
+})
+
+userRouter.get("/user/connections", authMiddleWare, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+        const connections = await ConnectionRequest.find({
+            $or: [
+                {
+                    toUserId: loggedInUser._id,
+                    status: "accepted"
+                },
+                {
+                    fromUserId: loggedInUser._id,
+                    status: "accepted"
+                },
+            ]
+
+        }).populate("fromUserId", "firstName lastName");
+
+        res.send({
+            message: "Fetched connections successfully",
+            data: connections
+        })
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+})
+
+userRouter.get("/feed", authMiddleWare, async (req, res) => {
+    try {
+        const page = req.query.page || 0;
+        const limit = req.query.limit || 10;
+
+        const skip = (page - 1) * limit;
+        const loggedInUser = req.user;
+
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                { fromUserId: loggedInUser._id, status: { $in: ["accepted", "interested"] } },
+                { toUserId: loggedInUser._id, status: { $in: ["accepted", "interested"] } }
+            ]
+        }).select("fromUserId toUserId");
+
+        const hideUsersFromFeed = new Set();
+
+        connectionRequests.forEach(req => {
+            hideUsersFromFeed.add(req.fromUserId.toString());
+            hideUsersFromFeed.add(req.toUserId.toString());
+        });
+
+        const users = await user.find({
+            $and: [
+                { _id: { $nin: Array.from(hideUsersFromFeed) } },
+                { _id: { $ne: loggedInUser._id } }
+            ]
+        }).select("firstName lastName age gender skills").skip(skip).limit(limit);
+
+        res.send({
+            message: "Fetched feed successfully",
+            data: users
+        })
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
 })
 
 module.exports = userRouter;
